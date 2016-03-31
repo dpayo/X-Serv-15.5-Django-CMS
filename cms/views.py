fbd9a82a1a7b8d5ebc4416fd2bec831b8a92b9fa
Diego Payo Martinez <dpayomartinez@gmail.com>
2016-03-31 21:19:06 +0200

    CMS Django

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from models import Pages
 # Create your views here.
+
+def show_cont(request):
+    respuesta="<ul>"
+    list_cont=Pages.objects.all()
+    for cont in list_cont :
+        respuesta+="<li>"+cont.name+" "+cont.page+"</li>"
+    respuesta+="</ul>"
+    return HttpResponse(respuesta)
+
+def save_cont(request,name,cont):
+    obj=Pages(name=name,page=cont)
+    obj.save()
+    return HttpResponse("<h1> Guardado.."+name+"</h1>")
+
+def get_cont(request,name):
+    try:
+        obj=Pages.objects.get(name=name)
+        return HttpResponse(obj.page)
+    except Pages.DoesNotExist:
+        return HttpResponse(" 404 Not Found")
+def error(request,cont1,cont2):
+    return HttpResponse("404 Not Found")
--- myproject/urls.py
+++ myproject/urls.py
@@ -5,6 +5,10 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'cms.views.show_cont'),
+    url(r'(.+)/(.+)', 'cms.views.save_cont'),
+    url(r'(.+)', 'cms.views.get_cont'),
+    url(r'(.*)/(.*)', 'cms.views.error'),
+
 )
